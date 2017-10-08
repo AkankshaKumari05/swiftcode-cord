c6dba7796a185be0261dbafaf9972d9dc3e0e02f
akanksha <akanksha5996@gmail.com>
2017-10-08 05:51:31 +0000

    styling

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,29 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+
+
+$scope.messages=[
+{
+'sender':'USER',
+'text':'hi',
+'time':'10:01 AM'
+},
+{
+'sender':'BOT',
+'text':'How do I help you?',
+'time':'10:01 AM'
+},
+{
+'sender':'USER',
+'text':'news about India',
+'time':'10:01 AM'
+},
+{
+'sender':'BOT',
+'text':'Invalid',
+'time':'10:02 AM'
+}]
+
+
 });
