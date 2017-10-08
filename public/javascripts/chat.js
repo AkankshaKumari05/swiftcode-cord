9d4468779727d6cda750140faacc50746e4474a1
akanksha <akanksha5996@gmail.com>
2017-10-08 10:59:25 +0000

    deploying

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,13 +1,13 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.config(function($mdThemingProvider) {
      $mdThemingProvider.theme('default')
-    .primaryPalette('pink', {
+    .primaryPalette('red', {
                 'default': '400', // by default use shade 400 from the pink palette for primary intentions
                 'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
                 'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
                 'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
             })
-            .accentPalette('green', {
+            .accentPalette('purple', {
                 'default': 'A400'
             })
             .dark();
