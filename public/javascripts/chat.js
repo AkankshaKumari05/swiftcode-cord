be5180987084d1af37cd0952a906fd1faf0489f6
akanksha <akanksha5996@gmail.com>
2017-10-08 11:11:52 +0000

    final code

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,13 +1,13 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.config(function($mdThemingProvider) {
      $mdThemingProvider.theme('default')
-    .primaryPalette('red', {
+    .primaryPalette('yellow', {
                 'default': '400', // by default use shade 400 from the pink palette for primary intentions
                 'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
                 'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
                 'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
             })
-            .accentPalette('purple', {
+            .accentPalette('blue', {
                 'default': 'A400'
             })
             .dark();
@@ -18,7 +18,7 @@ app.controller('chatController', function ($scope, $sce) {
 
 $scope.messages=[]
 
-var  exampleSocket =  new  WebSocket('ws://localhost:9000/chatSocket');
+var  exampleSocket =  new  WebSocket('wss://swiftcode-akanksha.herokuapp.com');
     exampleSocket.onmessage  =   function  (event) {
         var jsonData = JSON.parse(event.data);
         jsonData.time = new Date()
